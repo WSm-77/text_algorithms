d4f7cf1a69640a05dc1ef494343ed5709c1168ce
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2025-03-18 17:29:24 +0100

    Add solution to extract_links.py

--- lab1/extract_links.py
+++ lab1/extract_links.py
@@ -15,27 +15,29 @@ def extract_links(html: str) -> list[dict[str, str]]:
             - 'text': the text between <a> and </a> tags
     """
 
-    # TODO: Implement a regular expression pattern to extract links from HTML.
+    # Implement a regular expression pattern to extract links from HTML.
     # The pattern should capture three groups:
     # 1. The URL (href attribute value)
     # 2. The title attribute (which might not exist)
     # 3. The link text (content between <a> and </a> tags)
-    url_pattern = r"href=\"(?P<url>).*?\""
-    title_pattern = r"title=\"(?P<title>).*?\""
-    pattern = r"<a(?P<flags>.*?)>(?P<text>).*?</a>"
+    url_pattern = r"href=\"(?P<url>.*?)\""
+    title_pattern = r"title=\"(?P<title>.*?)\""
+    pattern = r"<a(?P<flags>.*?)>(?P<text>.*?)</a>"
 
-    for line in html.splitlines():
-        match = re.match(pattern, line)
+    links = []
 
-        if not match:
-            continue
+    # Use re.finditer to find all matches of the pattern in the HTML
+    # For each match, extract the necessary information and create a dictionary
+    # Then append that dictionary to the 'links' list
+    for a_tag_match in re.finditer(pattern, html, flags=re.DOTALL):
+        flags_match = a_tag_match.group("flags")
+        url_match = re.search(url_pattern, flags_match)
+        title_match = re.search(title_pattern, flags_match)
 
-        links = [link_match.group("url") for link_match in re.finditer(url_pattern, match.group("flags"))]
-
-        # TODO: Use re.finditer to find all matches of the pattern in the HTML
-        # For each match, extract the necessary information and create a dictionary
-        # Then append that dictionary to the 'links' list
-
-    print(links)
+        links.append({
+            "url" : url_match.group("url"),
+            "text" : a_tag_match.group("text"),
+            "title" : title_match.group("title") if title_match is not None else None
+        })
 
     return links
