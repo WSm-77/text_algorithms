a3fa1234cb40f2a4a2ed0d43eb9f4fdad6321ae4
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2025-04-02 11:37:05 +0200

    Clean kmp

--- lab2/lab_2/kmp_algorithm.py
+++ lab2/lab_2/kmp_algorithm.py
@@ -1,29 +1,3 @@
-def length_of_common_prefix(str1: str, str2: str) -> int:
-    min_len = min(len(str1), len(str2))
-    for i in range(min_len):
-        if str1[i] != str2[i]:
-            return i
-    return min_len
-
-def z_function(s: str):
-    z = [0] * len(s)
-    l = 0
-    r = 0
-    for k in range(1, len(s)):
-        if k >= r:
-            z[k] = length_of_common_prefix(s[k:], s)
-            if z[k] > 0:
-                l = k
-                r = k + z[k]
-        elif z[k - l] >= r - k:
-            z[k] = r - k + length_of_common_prefix(s[r:], s[r - k:])
-            l = k
-            r = k + z[k]
-        else:
-            z[k] = z[k - l]
-
-    return z
-
 def compute_lps_array(pattern: str) -> list[int]:
     """
     Compute the Longest Proper Prefix which is also Suffix array for KMP algorithm.
@@ -34,20 +8,12 @@ def compute_lps_array(pattern: str) -> list[int]:
     Returns:
         The LPS array
     """
-    # TODO: Implement the Longest Prefix Suffix (LPS) array computation
+    # Implement the Longest Prefix Suffix (LPS) array computation
     # The LPS array helps in determining how many characters to skip when a mismatch occurs
     # For each position i, compute the length of the longest proper prefix of pattern[0...i]
     # that is also a suffix of pattern[0...i]
     # Hint: Use the information from previously computed values to avoid redundant comparisons
 
-    # pattern_len = len(pattern)
-    # z_arr = compute_z_array(pattern)
-    # lps_arr = [0] * (pattern_len + 1)
-
-    # for i in range(pattern_len - 1, 0, -1):
-    #     lps_arr[i + z_arr[i]] = z_arr[i]
-
-    # return lps_arr
     lps = [0] * len(pattern)
     length = 0  # Length of the previous longest prefix suffix
     i = 1
@@ -65,14 +31,6 @@ def compute_lps_array(pattern: str) -> list[int]:
                 i += 1
 
     return lps
-
-    # z = z_function(pattern)
-    # p = [0] * (len(pattern) + 1)
-
-    # for j in range(len(pattern) - 1, 0, -1):
-    #      p[j + z[j]] = z[j]
-
-    # return p
 
 def kmp_pattern_match(text: str, pattern: str) -> list[int]:
     """
@@ -113,8 +71,3 @@ def kmp_pattern_match(text: str, pattern: str) -> list[int]:
             j = p[j - 1]
 
     return result
-
-if __name__ == "__main__":
-    pattern = "ABABACA"
-    res = compute_lps_array(pattern)
-    print(res)
